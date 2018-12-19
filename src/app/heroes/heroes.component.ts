1660011e11cf3f8b552d253f89afab4bbfc0d538
lyricszhang <lyricszhang@tencent.com>
2018-12-19 20:14:02 +0800

    message servicee

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { timingSafeEqual } from 'crypto';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,11 +9,21 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  heroes: Hero[];
+  selectedHero: Hero;
+  // 声明了一个私有 heroService 属性，2. 把它标记为一个 HeroService 的注入点。
+  constructor(private heroService: HeroService) { }
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
